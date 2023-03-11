775d22732257ed1e2a6a7d3952f3bb46eb870e22
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-03-11 17:26:22 +0100

    add

--- scripts.js
+++ scripts.js
@@ -3,11 +3,19 @@ console.log(CONNECT);
 const inputYear = '1991';
 
 // type conversions
-// Convertir string en number:
-console.log(Number(inputYear));
+// Convertir number:
+console.log(Number(inputYear), typeof(Number(inputYear)));
+// Convertir en string
+console.log(String(inputYear), typeof(String(inputYear)));
 
 //type coercion conversions
 console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' / 2);
+console.log('23' * 2);
+
+
 
 
 
