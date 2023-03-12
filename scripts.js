cc8f2fc19e6c959ffc56715d444f2d7d8038dc44
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-03-12 18:36:07 +0100

    add

--- scripts.js
+++ scripts.js
@@ -1,36 +1,15 @@
 const CONNECT = "Je suis connecté";
 console.log(CONNECT);
-const inputYear = '1991';
 
-// type conversions
-///////////////////
-// Convertir number:
-console.log(Number(inputYear), typeof(Number(inputYear)));
-// Convertir en string
-console.log(String(inputYear), typeof(String(inputYear)));
+//Ternary
+const age = 15;
+age >= 18 ? console.log("I like to drink wine") : 
+console.log ("No way. Drink water !");
 
-//type coercion conversions
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' + '10' + 3);
-console.log('23' / 2);
-console.log('23' * 2);
+const drink = age >= 18 ? console.log("I like to drink wine") : 
+console.log ("No way. Drink water !");
 
-// 5 falsy values : 0, '', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined)); 
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
-
-let height = 0;
-if (height){
-    console.log("height is " + height);
-}else{
-    console.log("pas de height");//le zéro est falsy
-}
-
-
+console.log(drink);
 
 
 
