48307e4209e68908113b8f32bb25146a2336a7e0
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-03-12 18:40:42 +0100

    add

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@ console.log(CONNECT);
 
 //Ternary
 const age = 15;
-age >= 18 ? console.log("I like to drink wine") : 
-console.log ("No way. Drink water !");
+// age >= 18 ? console.log("I like to drink wine") : 
+// console.log ("No way. Drink water !");
 
-const drink = age >= 18 ? console.log("I like to drink wine") : 
-console.log ("No way. Drink water !");
+// const drink = age >= 18 ? console.log("I like to drink wine") : 
+// console.log ("No way. Drink water !");
 
-console.log(drink);
+//console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
 
 
